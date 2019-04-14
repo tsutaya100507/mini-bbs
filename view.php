efd259e451147cd8d1fea118109451df2c85ee62
Tatsuya Ichikawa <>
2019-04-15 00:29:24 +0900

    [Add] Add post detail page

--- view.php
+++ view.php
@@ -1,3 +1,17 @@
+<?php
+  session_start();
+  require('dbconnect.php');
+
+  if (empty($_REQUEST['id'])) {
+    header('Location: index.php');
+    exit();
+  }
+
+  $posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+  $posts->execute(array($_REQUEST['id']));
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -17,14 +31,16 @@
   <div id="content">
   <p>&laquo;<a href="index.php">一覧にもどる</a></p>
 
+  <?php if ($post = $posts->fetch()): ?>
     <div class="msg">
-    <img src="member_picture/" />
-    <p><span class="name">（）</span></p>
-    <p class="day"></p>
+    <img src="member_picture/<?php print(htmlspecialchars($post['picture'])); ?>" width="400px" height="400px" />
+    <p><?php print(htmlspecialchars($post['message'])); ?><span class="name">（<?php print(htmlspecialchars($post['name'])); ?>）</span></p>
+    <p class="day"><?php print(htmlspecialchars($post['created'])); ?></p>
     </div>
-
-	<p>その投稿は削除されたか、URLが間違えています</p>
-  </div>
+  <?php else: ?>
+    <p>その投稿は削除されたか、URLが間違えています</p>
+  <?php endif; ?>
+    </div>
 </div>
 </body>
 </html>
